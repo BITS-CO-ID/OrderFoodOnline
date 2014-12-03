e58f1970ecb87a4d3cfbf91bf3fc9d1ecb9f2b0b
makzr <markus.koelzer@gmail.com>
2014-12-03 10:04:56 +0100

    Renaming

--- Backend/FoodDeliveryBackend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FoodDeliveryBackend.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
---
+++ Backend/OrderFoodOnlineBackend/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FoodDeliveryBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
